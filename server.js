335c9c3d85cac4f2370fdcbff25857b19b08232b
nuvallo <nuvallo.inc@gmail.com>
2020-04-04 16:08:56 -0400

    setting up CRUD and routes

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactHolder API" })
 );
 
+// Defined Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
